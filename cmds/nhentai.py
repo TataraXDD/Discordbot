02f27f38171c1f47dbaad266ba46bff0d8cf0506
TataraXDD <aa3452544215@yahoo.com.tw>
2021-12-30 13:13:22 +0800

    nhentai

--- cmds/nhentai.py
+++ cmds/nhentai.py
@@ -1,10 +1,25 @@
 from discord.ext import commands
 from core.classess import Cog_Extension as CE
+import re
 
 class nhentai(CE):
 
 
     @commands.command()
-    async def n(self,ctx):
+    async def n(self,ctx,num):
         base_url = "https://nhentai.net/g/"
-        await ctx.send("base_ur/l"+ctx)
+        rnum = re.compile(r'\d\d\d\d\d\d')
+        booknum = rnum.search(num)
+        url = base_url+booknum.group()
+        await ctx.send(url)
+
+
+
+
+
+
+
+
+
+def setup(bot):
+    bot.add_cog(nhentai(bot))
